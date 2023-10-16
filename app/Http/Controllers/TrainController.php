02e4d28fce0c82179236ae922432294648fabf71
AlessandroIacovella95 <alessandroiacovella95@gmail.com>
2023-10-16 17:31:47 +0200

    add controller

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Train;
+
 use Illuminate\Http\Request;
 
 class TrainController extends Controller
 {
-    //
+    public function index()
+  {
+    $title = "Train";
+    $trains= Train::where('data_di_partenza', '=', '2023-10-16')->get();
+    return view('train', compact('title','trains'));
+  }
 }
